dedf5c5f86dd76e582acbbc245ace80bd9043f3d
poomkingzaa <77338052+poomkingzaa@users.noreply.github.com>
2021-01-12 21:50:06 +0700

    Update hello.java
    2 hello

--- test/hello.java
+++ test/hello.java
@@ -1,6 +1,6 @@
 package test;
 public class hello{
     public static void main(String[] args) {
-        System.out.print("hello from poom");
+        System.out.print("hello from poom at CAMT");
     }
 }
